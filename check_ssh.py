4342dcac10dca66885c70be46412a612fdf2b6b4
Al Johri <al.johri@gmail.com>
2014-05-27 10:38:15 -0500

    Added several more servers to servers.txt

--- check_ssh.py
+++ check_ssh.py
@@ -6,10 +6,19 @@ servers = open('servers.txt').read().splitlines()
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+counter = 0
+
+print ""
+
 for server in servers:
     try:
         ssh.connect(server, username='asj936', timeout=0.5)
     except Exception as e:
         print server.ljust(45) + colorama.Fore.RED + str(e)
         continue
+    counter += 1
     print server.ljust(45) + colorama.Fore.GREEN + "okay"
+
+print "======================================================"
+print "Active Servers: " + str(counter)
+print "Total Servers: " + str(len(servers))
---
+++ monitor.py
@@ -0,0 +1,26 @@
+from celery import Celery
+
+def my_monitor(app):
+    state = app.events.State()
+
+    def announce_failed_tasks(event):
+        state.event(event)
+        # task name is sent only with -received event, and state
+        # will keep track of this for us.
+        task = state.tasks.get(event['uuid'])
+
+        print('TASK FAILED: %s[%s] %s' % (
+            task.name, task.uuid, task.info(), ))
+
+    with app.connection() as connection:
+        recv = app.events.Receiver(connection, handlers={
+                # 'task-failed': announce_failed_tasks,
+                '*': state.event,
+        })
+        recv.capture(limit=None, timeout=None, wakeup=True)
+
+if __name__ == '__main__':
+    import os
+    rabbitmq = os.getenv('BROKER_URL')
+    app = Celery(broker=rabbitmq, backend=rabbitmq)
+    my_monitor(app)
