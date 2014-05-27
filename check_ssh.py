bad084aa3976caf6aa488825f3202c1b33ec93e7
Al Johri <al.johri@gmail.com>
2014-05-27 11:32:43 -0500

    Check python version and location.

--- check_ssh.py
+++ check_ssh.py
@@ -1,5 +1,8 @@
-import paramiko, colorama
+import paramiko, colorama, select, socket
 colorama.init(autoreset=True)
+
+from colorama import Fore
+from paramiko import AuthenticationException, SSHException, BadHostKeyException
 
 servers = open('servers.txt').read().splitlines()
 
@@ -13,11 +16,20 @@ print ""
 for server in servers:
     try:
         ssh.connect(server, username='asj936', timeout=0.5)
-    except Exception as e:
-        print server.ljust(45) + colorama.Fore.RED + str(e)
+        (stdin1, stdout1, stderr1) = ssh.exec_command('which python')
+        python_location = stdout1.read().strip()
+
+        # python --version wasn't working for god knows what reason
+        (stdin2, stdout2, stderr2) = ssh.exec_command('python -c "import sys; print sys.version[:6]"')
+        python_version = stdout2.read().strip()
+
+        ssh.close()
+    except (AuthenticationException, SSHException, BadHostKeyException, socket.error) as e:
+        print server.ljust(45) + Fore.RED + str(e).ljust(20)
         continue
+
     counter += 1
-    print server.ljust(45) + colorama.Fore.GREEN + "okay"
+    print server.ljust(45) + Fore.GREEN + "okay".ljust(10) + Fore.RESET + python_location.ljust(30) + python_version
 
 print "======================================================"
 print "Active Servers: " + str(counter)
